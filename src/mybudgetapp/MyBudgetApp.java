efb830dca94ad5ae0ff3107b16769b7ed0bb6f65
shrooq <shoroqalsadiq@gmail.com>
2024-10-01 14:32:39 +0300

    shrooq

--- src/mybudgetapp/MyBudgetApp.java
+++ src/mybudgetapp/MyBudgetApp.java
@@ -15,7 +15,7 @@ public class MyBudgetApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("shrooq");
     }
     
 }
