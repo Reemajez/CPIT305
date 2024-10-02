b8c8db3a8c216b6601b1b61f3de2b11a4463cb89
Reema <reemajezz@hotmail.com>
2024-10-02 17:59:33 +0300

    this push for Reema!

---
+++ src/main/java/project_305.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author reemajez
+ */
+public class project_305 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.println("Hello this is Reema");
+    }
+    
+}
